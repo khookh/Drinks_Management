a210649253287b080db8f5a81dfe61f00e3e875f
sdonne <stefanodonne@gmail.com>
2020-04-03 00:01:44 +0200

    Add Consumption and Overview tabs

--- app/src/main/java/Controller/Fragment/SectionsPagerAdapter.java
+++ app/src/main/java/Controller/Fragment/SectionsPagerAdapter.java
@@ -1,6 +1,5 @@
 package Controller.Fragment;
 
-import android.content.Context;
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.fragment.app.Fragment;
@@ -8,37 +7,42 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import com.example.defonce_management.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A [FragmentPagerAdapter] that returns a fragment corresponding to
  * one of the sections/tabs/pages.
  */
 public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+
     @StringRes
     private static final int[] TAB_TITLES = new int[]{R.string.Alcool_Consumption, R.string.Overview};
-    private final Context mContext;
 
-    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+    public SectionsPagerAdapter(FragmentManager fm) {
         super(fm);
-        mContext = context;
+    }
+    public void addFragment(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
     }
 
     @Override
     public Fragment getItem(int position) {
-        // getItem is called to instantiate the fragment for the given page.
-        // Return a PlaceholderFragment (defined as a static inner class below).
-        return PlaceholderFragment.newInstance(position + 1);
+        return mFragmentList.get(position);
     }
 
     @Nullable
     @Override
     public CharSequence getPageTitle(int position) {
-        return mContext.getResources().getString(TAB_TITLES[position]);
+        return mFragmentTitleList.get(position);
     }
-
     @Override
     public int getCount() {
-        // Show 2 total pages.
-        return 2;
+        return mFragmentList.size();
     }
+
 }
--- app/src/main/java/Controller/Session_Control.java
+++ app/src/main/java/Controller/Session_Control.java
@@ -1,7 +1,8 @@
 package Controller;
 
+import Controller.Fragment.Alcool_Consumption;
 import Controller.Fragment.SectionsPagerAdapter;
-import Model.User;
+import Controller.Fragment.Overview;
 import android.os.Bundle;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,8 +15,7 @@ import com.google.android.material.tabs.TabLayout;
 public class Session_Control extends AppCompatActivity {
 
 
-    User actual_user = WelcomePage.getActual_user();
-    TextView startup;
+    TextView sectionlabel;
     TabLayout tabLayout;
     ViewPager viewPager;
 
@@ -23,19 +23,24 @@ public class Session_Control extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.session);
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
         ViewPager viewPager = findViewById(R.id.view_pager);
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
         FloatingActionButton fab = findViewById(R.id.fab);
-        //startup = (TextView) findViewById(R.id.startup);
+
+        sectionsPagerAdapter.addFragment(new Alcool_Consumption(), "Alcool Consumption");
+        sectionsPagerAdapter.addFragment(new Overview(), "Overview");
+        viewPager.setAdapter(sectionsPagerAdapter);
+        tabs.setupWithViewPager(viewPager);
+
         startup();
 
     }
 
     public void startup(){
-        //startup.setText("Welcome "+actual_user.getName()+" !" +"\n"+"Your alcool blood level is now around "+actual_user.getAlcoolRate()+" g/L");
+        // sectionlabel.setText("Hello "+actual_user.getName()+" !" +"\n"+"Your alcool blood level is now around "+actual_user.getAlcoolRate()+" g/L");
     }
 
 }
