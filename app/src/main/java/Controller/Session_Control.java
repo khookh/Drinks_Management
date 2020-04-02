05ba27ccbdc7a5928e17821f7016e402839d37fa
sdonne <stefanodonne@gmail.com>
2020-04-02 23:20:44 +0200

    Add fragments and viewmodel

--- app/src/main/java/Controller/Session_Control.java
+++ app/src/main/java/Controller/Session_Control.java
@@ -1,23 +1,41 @@
 package Controller;
 
+import Controller.Fragment.SectionsPagerAdapter;
 import Model.User;
 import android.os.Bundle;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 import com.example.defonce_management.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
+
 
 public class Session_Control extends AppCompatActivity {
+
+
     User actual_user = WelcomePage.getActual_user();
     TextView startup;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.session);
-        startup = (TextView) findViewById(R.id.startup);
-
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        //startup = (TextView) findViewById(R.id.startup);
         startup();
+
     }
+
     public void startup(){
-        startup.setText("Welcome "+actual_user.getName()+" !" +"\n"+"Your alcool blood level is now around "+actual_user.getAlcoolRate()+" g/L");
+        //startup.setText("Welcome "+actual_user.getName()+" !" +"\n"+"Your alcool blood level is now around "+actual_user.getAlcoolRate()+" g/L");
     }
 
 }
