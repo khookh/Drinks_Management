0fad472d171d565b368049ac3102a7f83fde9e51
Darius Couchard <darius.couchard@hotmail.com>
2020-04-12 21:41:00 +0200

    Started ConsumptionService

---
+++ web/src/main/java/services/ConsumeService.java
@@ -0,0 +1,31 @@
+package services;
+
+/**
+ * This services takes care of the consumptions
+ * of a custom drink. The management of custom
+ * drinks etc is in the {@link DrinkService}.
+ *
+ * @author GriffinBabe
+ */
+public class ConsumeService implements Service {
+
+    @Override
+    public void start() throws CantStartServiceException {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+}
